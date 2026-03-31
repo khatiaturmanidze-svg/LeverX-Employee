1d5733df0b9d1a53410fd7eec4d718102d75c301
khatia turmanidze <khatia.turmanidze@leverx.com>
2026-03-31 20:09:04 +0400

    wip: partial responsiveness for address page

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// import path from 'node:path';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
